d1d9b7fc0f5ae6e2bfc6c491e85148f4ea130ddc
gyip40 <gyip40@stuy.edu>
2023-06-04 16:19:51 -0400

    demo1

---
+++ Boba.java
@@ -0,0 +1,69 @@
+//package final_project;
+
+public class Boba {
+
+    private int size;
+    private int iceLevel;
+    private int sugarLevel;
+    private String drink;
+    private String topping;
+
+    public Boba() {
+        size = 0;
+        iceLevel = 0;
+        sugarLevel = 0;
+        drink = "";
+        topping = "";
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getIceLevel() {
+        return iceLevel;
+    }
+
+    public void setIceLevel(int iceLevel) {
+        this.iceLevel = iceLevel;
+    }
+    
+    public int getSugarLevel() {
+        return sugarLevel;
+    }
+
+    public void setSugarLevel(int sugarLevel) {
+        this.sugarLevel = sugarLevel;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+    public String getTopping() {
+        return topping;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+
+    public static String toString(Boba boba) {
+        String result = "";
+        result += "SIZE:          " + boba.getSize() + "\n";
+        result += "ICE LEVEL:     " + boba.getIceLevel() + "\n";
+        result += "SUGAR LEVEL:   " + boba.getSugarLevel() + "\n";
+        result += "DRINK:         " + boba.getDrink() + "\n";
+        result += "TOPPING:       " + boba.getTopping() + "\n";
+        return result;
+    }
+
+}
