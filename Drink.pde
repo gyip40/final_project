b3e7c5d1096c06bf599855b733ac81b9a5912ee5
gyip40 <gyip40@stuy.edu>
2023-06-04 22:02:05 -0400

    update demo3

--- Drink.pde
+++ Drink.pde
@@ -1,4 +1,105 @@
 public class Drink {
-    
-    int 
+  private Menu menu;
+  private Boba boba;
+  private String drinkColor;
+  private String together;
+  
+  public Drink() {
+    menu = new Menu();
+    menu.generateBoba();
+    boba = new Boba();
+    boba = menu.getBoba();
+    drinkColor = "";
+    together = "";
+  }
+  
+  public Boba getBoba() {
+    return boba;
+  }
+  
+  public void translate(int x, int y) {
+    textAlign(CENTER, CENTER);
+    String translation = "";
+    switch (boba.getSize()) {
+      case 1:
+        translation += "small";
+        break;
+      case 2:
+        translation += "medium";
+        break;
+      case 3:
+        translation += "large";
+        break;
+    }
+    switch (boba.getDrink()) {
+      case "oolong tea":
+        drinkColor += "#da7800";
+        together += "#d7a96b";
+        break;
+      case "black tea":
+        drinkColor += "#971e09";
+        together += "#9d493a";
+        break;
+      case "milk tea":
+        drinkColor += "#c6945f";
+        together += "#be966b";
+        break;
+      case "green tea":
+        drinkColor += "#55740d";
+        together += "#7e914a";
+        break;
+      case "fresh milk":
+        drinkColor += "#ded7c6";
+        together += "#f7efdf";
+        break;
+    }
+    translation += " " + boba.getDrink() + "\nwith ";
+    fill(0);
+    translation += boba.getTopping() + ",\n";
+    switch (boba.getIceLevel()) {
+      case 0:
+        translation += "no";
+        break;
+      case 25:
+        translation += "less";
+        break;
+      case 50:
+        translation += "regular";
+        break;
+    }
+    translation += " ice,\n";
+    switch (boba.getSugarLevel()) {
+      case 0:
+        translation += "no";
+        break;
+      case 25:
+        translation += "half";
+        break;
+      case 50:
+        translation += "regular";
+        break;
+      case 75:
+        translation += "extra";
+        break;
+    }
+    translation += " sugar";
+    text(translation, x, y);
+  }
+  
+  void drawDrink() {
+    strokeWeight(3);
+    strokeJoin(ROUND);
+    strokeCap(ROUND);
+    stroke(255, 80);
+    fill(255, 5);
+    if (boba.getSize() == 1) {
+      quad(400, 725, 600, 725, 635, 400, 365, 400);
+    }
+    if (boba.getSize() == 2) {
+      quad(400, 725, 600, 725, 642, 335, 358, 335);
+    }
+    else {
+      quad(400, 725, 600, 725, 649, 270, 351, 270);
+    }
+  }
 }
--- Menu.java
+++ Menu.java
@@ -7,40 +7,44 @@ public class Menu {
     private String[] toppingSelection;
     
     public Menu() {
-        sizes = new int[] {1, 2, 3};
-        iceLevels = new int[] {0, 25, 50, 75};
-        sugarLevels = new int[] {0, 25, 50, 75};
-        drinkSelection = new String[] {"Oolong tea", "Black tea", "Milk tea", "Green tea", "Fresh milk"};
-        toppingSelection = new String[] {"Pearls", "Auyu Jelly", "Grass Jelly", "Cheese foam"};
+      sizes = new int[] {1, 2, 3};
+      iceLevels = new int[] {0, 25, 50};
+      sugarLevels = new int[] {0, 25, 50, 75};
+      drinkSelection = new String[] {"oolong tea", "black tea", "milk tea", "green tea", "fresh milk"};
+      toppingSelection = new String[] {"pearls", "auyu jelly", "grass jelly"};
     }
-
+    
+    public Boba getBoba() {
+      return randomDrink;
+    }
+    
     public void generateSize() {
         int randomIndex = (int)(Math.random() * sizes.length);
-        // System.out.println(randomIndex); //debugging
+        //System.out.println(randomIndex); //debugging
         randomDrink.setSize(sizes[randomIndex]);
     }
 
     public void generateIceLevel() {
         int randomIndex = (int)(Math.random() * iceLevels.length);
-        // System.out.println(randomIndex); //debugging
+        //System.out.println(randomIndex); //debugging
         randomDrink.setIceLevel(iceLevels[randomIndex]);
     }
     
     public void generateSugarLevel() {
         int randomIndex = (int)(Math.random() * sugarLevels.length);
-        // System.out.println(randomIndex); //debugging
+        //System.out.println(randomIndex); //debugging
         randomDrink.setSugarLevel(sugarLevels[randomIndex]);
     }
     
     public void generateDrink() {
         int randomIndex = (int)(Math.random() * drinkSelection.length);
-        // System.out.println(randomIndex); //debugging
+        //System.out.println(randomIndex); //debugging
         randomDrink.setDrink(drinkSelection[randomIndex]);
     }
     
     public void generateTopping() {
         int randomIndex = (int)(Math.random() * toppingSelection.length);
-        // System.out.println(randomIndex); //debugging
+        //System.out.println(randomIndex); //debugging
         randomDrink.setTopping(toppingSelection[randomIndex]);
     }
 
@@ -50,12 +54,7 @@ public class Menu {
         generateSugarLevel();
         generateDrink();
         generateTopping();
-        // System.out.println("-----------------------------------------");
-        // System.out.println(Boba.toString(randomDrink));
+        System.out.println("-----------------------------------------");
+        System.out.println(randomDrink.toString());
     }
-
-    // public static void main(String[] args) {
-    //     Menu test = new Menu();
-    //     test.generateBoba(); 
-    // }
 }
